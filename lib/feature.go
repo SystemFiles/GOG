885d491a2ea65eb14bdbe0dd78d9f8846a0e7ece
SystemFiles <bensykes12@gmail.com>
2022-03-12 14:17:46 -0500

    BENS-0001 WIP: checkpoint

--- lib/feature.go
+++ lib/feature.go
@@ -1,10 +1,11 @@
 package lib
 
 type Feature struct {
-	Jira string `yaml:"jira"`
-	Comment string `yaml:"comment"`
+	Jira string `json:"jira"`
+	Comment string `json:"comment"`
 }
 
+// 1. Create feature object
 func NewFeature(jira, comment string) (*Feature, error) {
 	feat := &Feature{Jira: jira, Comment: comment}
 
