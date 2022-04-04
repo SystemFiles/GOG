5f2b9a439fb03dc2a0c828d8660be214459fafa8
SystemFiles <bensykes12@gmail.com>
2022-04-04 14:28:39 -0400

    BENS-0013 created install script and instructions

---
+++ INSTALL.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+# Installation Script For GOG on Linux/Darwin
+
+set -e
+
+GOG_VERSION="0.3.1"
+
+if [[ -f "$HOME/bin/gog" ]]; then
+  echo "GOG is already installed."
+  exit 0
+fi
+
+if [ ! command -v tar &> /dev/null ]; then
+  echo "required command, tar, could not be found"
+  exit 1
+fi
+
+if [ ! command -v curl &> /dev/null ]; then
+  echo "required command, curl, could not be found"
+  exit 1
+fi
+
+[ "$(uname -s)" == "Darwin" ] && INSTALL_OS="darwin"
+[ "$(uname -s)" == "Linux"] && INSTALL_OS="linux"
+
+if [[ -z $INSTALL_OS ]]; then
+  echo "Current OS not supported by installation script ... exiting!"
+  exit 1
+fi
+
+[ "$(uname -m)" == "x86_64" ] && INSTALL_ARCH="amd64"
+[ "$(uname -m)" == "armv7l" ] && INSTALL_ARCH="arm64"
+[ "$(uname -m)" == "i386"] && INSTALL_ARCH="386"
+
+if [[ -z $INSTALL_ARCH ]]; then
+  echo "Current OS Architecture not supported by installation script ... exiting!"
+  exit 1
+fi
+
+INSTALL_FILE="GOG-${GOG_VERSION}-${INSTALL_OS}-${INSTALL_ARCH}.tar.gz"
+
+if [[ ! -d "$HOME/bin" ]]; then
+  mkdir "$HOME/bin"
+fi
+
+if [[ ! -d "$HOME/tmp" ]]; then
+  mkdir "$HOME/tmp"
+fi
+
+cd $HOME/tmp
+curl -LO "https://github.com/SystemFiles/GOG/releases/download/${GOG_VERSION}/${INSTALL_FILE}"
+tar -zxvf "./${INSTALL_FILE}"
+mv ./gog $HOME/bin/gog
+cd; rm -rf $HOME/tmp/
+
+$HOME/bin/gog -v
+if [[ $? -ne 0 ]]; then
+  echo "Installation Failed!"
+  exit 1
+fi
--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # GOG - Opinionated Git
 
-GoG is the Golang implementation of OG (AKA: Opinionated Git)
+GOG is an Opinionated Git workflow CLI tool written in Golang to help developers enforce consistency and strong project/version management within their projects without the added complexity of manual process documentation. Rather GOG allows you to focus on developing features and bringing value to stakeholders rather than managing versions, dependencies and CHANGELOGs.
 
-## Usage
+## Installation
+
+### Linux / MAC OSX
+
+```bash
+
+curl -s https://raw.githubusercontent.com/SystemFiles/GOG/master/INSTALL.sh | bash /dev/stdin
+
+```
+
+### Windows
+
+TBD
+
+## Basic Usage
 
 ```bash
 
